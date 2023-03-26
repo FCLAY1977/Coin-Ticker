eb4f6f53370fd13fc5008f9704e1102ed824a053
Fred Clay <fred.clay1977@gamil.com>
2023-03-26 12:31:44 -0400

    updated logic

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-.then(res => {
-    if (res.ok) {
-      console.log('SUCCESS') 
+// Define API URL
+const apiUrl = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 
-    } else{
-      console.log("Not Successful") 
+// Fetch Bitcoin prices from API
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Extract price data from API response
+    const usdPrice = data.bpi.USD.rate;
+    const gbpPrice = data.bpi.GBP.rate;
+    const eurPrice = data.bpi.EUR.rate;
 
-    }
-
-
-
-    res.json()
-}) 
-       
-    
-.then(data => console.log(data))
-.catch(error => console.log('ERROR'))
+    // Display price data in HTML table
+    const tableBody = document.getElementById('table-body');
+    tableBody.innerHTML = `
+      <tr>
+        <td>USD</td>
+        <td>${usdPrice}</td>
+      </tr>
+      <tr>
+        <td>GBP</td>
+        <td>${gbpPrice}</td>
+      </tr>
+      <tr>
+        <td>EUR</td>
+        <td>${eurPrice}</td>
+      </tr>
+    `;
+  })
+  .catch(error => console.error(error));
